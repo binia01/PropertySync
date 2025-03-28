cc3abd47a3a081ad9ec6e51a3756ee664031a918
binia01 <176568730+binia01@users.noreply.github.com>
2025-03-28 14:38:44 +0300

    added config

--- mobile_backend/src/app.module.ts
+++ mobile_backend/src/app.module.ts
@@ -5,9 +5,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { EventModule } from './event/event.module';
 import { VenueModule } from './venue/venue.module';
 import { BookingModule } from './booking/booking.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule, EventModule, VenueModule, BookingModule],
-
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+    AuthModule, 
+    UserModule,
+    PrismaModule, 
+    EventModule, 
+    VenueModule, 
+    BookingModule
+  ],
 })
 export class AppModule {}
