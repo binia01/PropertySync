9fc1e067d99d9428c4df6442d6c5d7fd05065ecd
Binyam Mezemir <binyam.mezemir-ug@aau.edu.et>
2025-04-26 10:58:02 +0300

    inital push and  navigation

---
+++ app/src/main/java/com/github/propertysync/pages/AppointmentsPage.kt
@@ -0,0 +1,14 @@
+package com.github.propertysync.pages
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun AppointmentsPage() {
+    Column() {
+        Text("This is for Appointments page")
+    }
+}
---
+++ app/src/main/java/com/github/propertysync/pages/ProfilePage.kt
@@ -0,0 +1,14 @@
+package com.github.propertysync.pages
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun ProfilePage() {
+    Column() {
+        Text("This is for Profile page")
+    }
+}
