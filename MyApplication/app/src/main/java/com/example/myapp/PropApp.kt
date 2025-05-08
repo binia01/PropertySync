16e79af80aa2442ec248a34b0e6ae8010dad6bca
Kibreab Chanyalew <affablemelon@gmail.com>
2025-05-08 14:40:04 +0300

    Revert "Merge pull request #4 from kidxcudi/Profile"
    This reverts commit 85ab88014c9046784163e47e890524fd6ed563e3, reversing
    changes made to 92b0545f4d57c5d92242e012e609f7ea06753965.
    
    revert before merge?

--- MyApplication/app/src/main/java/com/example/myapp/PropApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PropApp  : Application(){
-
-}
--- mobile_backend/src/appointment/dto/update-appointment-status.dto.ts
+++
@@ -1,8 +0,0 @@
-import { AppointmentStatus } from "@prisma/client";
-import { IsEnum } from "class-validator";
-
-export class updateAppointmentStatusDto{
-    
-    @IsEnum(AppointmentStatus)
-    status: AppointmentStatus;
-}
