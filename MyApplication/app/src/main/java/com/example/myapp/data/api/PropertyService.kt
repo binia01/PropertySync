c51f841d7fc2118015b82da3af480be2ea37f871
abnsol <abenisolo51196@gmail.com>
2025-05-09 21:49:02 +0300

    Add add property post request

--- MyApplication/app/src/main/java/com/example/myapp/data/api/PropertyService.kt
+++ MyApplication/app/src/main/java/com/example/myapp/data/api/PropertyService.kt
@@ -1,13 +1,21 @@
 package com.example.myapp.data.api
 
+import com.example.myapp.data.model.Request.AddPropertyRequest
+import com.example.myapp.data.model.Response.AddPropertyResponse
 import com.example.myapp.data.model.ReturnProperty
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface PropertyService {
     @GET("property")
     suspend fun getProperties(@Header("Authorization") token:String): Response<List<ReturnProperty>>
+
+    @POST("property")
+    suspend fun addProperty(@Header("Authorization") token:String, @Body property: AddPropertyRequest): Response<AddPropertyResponse>
+
 }
 
 
