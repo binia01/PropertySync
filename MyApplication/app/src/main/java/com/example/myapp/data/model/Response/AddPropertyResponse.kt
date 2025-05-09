a4a6e5c7980d04c96540f17735687f7806f8660a
abnsol <abenisolo51196@gmail.com>
2025-05-09 21:52:14 +0300

    Create custom add property response obj

---
+++ MyApplication/app/src/main/java/com/example/myapp/data/model/Response/AddPropertyResponse.kt
@@ -0,0 +1,8 @@
+package com.example.myapp.data.model.Response
+
+import com.example.myapp.data.model.Property
+
+data class AddPropertyResponse (
+    val message : String,
+    val property : PropertyResponse
+)
